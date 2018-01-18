139aef049848c9040ee23c22b0b809d72583fae8
Vincent Moore <cosmiqwarrior@gmail.com>
2018-01-17 22:10:00 -0500

    Updated app.jsx file

--- react/app.jsx
+++ react/app.jsx
@@ -1,27 +1,35 @@
-// create our Button component as a functional component
-const Button = (props) => {
-  return (
-    <input
-      type="button"
-      className={props.type === 'action' ? 'button action-button' : 'button input-button'}
-      onClick={props.handleClick}
-      value={props.label}
-    />
+const appRoot = document.getElementById('root');
+
+const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const r = 23
+    const fp = 3 / 100;
+    const ne = 4;
+    const fl = 5 / 100;
+    const fi = 5 / 100;
+    const fc = 5 / 100;
+    const l = 1000000;
+
+    alert(Math.floor(r * fp * ne * fl * fi * fc * l) + " detectable civilizations in the Milky Way.");
+
+};
+
+const renderForm = () => {
+  const template = (
+    <form onSubmit={onFormSubmit}>
+        <input type="text" id="R" name="R" value="" placeholder="R* - Annual formation rate of suitable stars in the Milky Way" />
+        <input type="text" id="Fp" name="Fp" value="" placeholder="Fp - Percentage of stars that have planets" />
+        <input type="text" id="Ne" name="Ne" value="" placeholder="Ne - Number of planets per star that can support life" />
+        <input type="text" id="Fl" name="Fl"  value="" placeholder="Fl - Percentage of planets where life emerges" />
+        <input type="text" id="Fi" name="Fi" value="" placeholder="Fi - Percentage of planets where intelligent life evolves" />
+        <input type="text" id="Fc" name="Fc" value="" placeholder="Fc - Percentage of civilizations that will develop communication" />
+        <input type="text" id="L" name="L" value="" placeholder="L - Years a civilization will last" />
+        <input type="submit" value="Calculate" />
+    </form>
   );
+
+  ReactDOM.render(template, appRoot);
 }
 
-
-ReactDOM.render(
-<form action="#">
-  <input type="text" id="R" name="field1" value="" placeholder="R* - Annual formation rate of suitable stars in the Milky Way" />
-  <input type="text" id="Fp" name="field2" value="" placeholder="Fp - Percentage of stars that have planets" />
-  <input type="text" id="Ne" name="field3" value="" placeholder="Ne - Number of planets per star that can support life" />
-  <input type="text" id="Fl" name="field3"  value=""placeholder="Fl - Percentage of planets where life emerges" />
-  <input type="text" id="Fi" name="field3" value="" placeholder="Fi - Percentage of planets where intelligent life evolves" />
-  <input type="text" id="Fc" name="field3" value="" placeholder="Fc - Percentage of civilizations that will develop communication" />
-  <input type="text" id="L" name="field3" value="" placeholder="L - Years a civilization will last" />
-  <input type="submit" value="Calculate" />
-</form>,
-
-  document.getElementById('root')
-);
+renderForm();
