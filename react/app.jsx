c6783890dd9e831e63625f5f66549969f84a14fb
Vincent Moore <cosmiqwarrior@gmail.com>
2018-03-23 11:32:24 -0400

    Updated JSX file
    Added onChange and conditionals to JSX file

--- react/app.jsx
+++ react/app.jsx
@@ -31,29 +31,41 @@ class DrakeForm extends React.Component {
       e.preventDefault();
 
       //Drake variables
-      const r = 23
-      const fp = 56 / 100;
-      const ne = 100;
-      const fl = 5 / 100;
-      const fi = 5 / 100;
-      const fc = 5 / 100;
-      const l = 1000000;
-      
+      const r  = document.getElementById('R').value;
+      const fp = document.getElementById('Fp').value / 100;
+      const ne = document.getElementById('Ne').value;
+      const fl = document.getElementById('Fl').value / 100;
+      const fi = document.getElementById('Fi').value / 100;
+      const fc = document.getElementById('Fc').value / 100;
+      const l  = document.getElementById('L').value;
+
+      //Equation solutions      
       const solve = Math.floor(r * fp * ne * fl * fi * fc * l);
       const calculate = document.querySelector('#calculate');
-      calculate.innerHTML =   `N = ${solve.toLocaleString()} detectable civilizations in the Milky Way.`;
+
+      //Result conditionals
+      if (solve == 0) {
+        calculate.innerHTML = `Unable to solve equation. Please try again.`;
+      } else if (solve == 1) {
+        calculate.innerHTML = `N = ${solve.toLocaleString()} detectable civilization in the Milky Way.`;
+      } else {
+        calculate.innerHTML = `N = ${solve.toLocaleString()} detectable civilizations in the Milky Way.`;
+      }
+
+      //Error handling
+      
   };
 
   render() {
     return (
       <form onSubmit={this.onFormSubmit}>
-          <input type="text" id="R" name="R" value="" placeholder="R* - Annual formation rate of suitable stars in the Milky Way" />
-          <input type="text" id="Fp" name="Fp" value="" placeholder="Fp - Percentage of stars that have planets" />
-          <input type="text" id="Ne" name="Ne" value="" placeholder="Ne - Number of planets per star that can support life" />
-          <input type="text" id="Fl" name="Fl"  value="" placeholder="Fl - Percentage of planets where life emerges" />
-          <input type="text" id="Fi" name="Fi" value="" placeholder="Fi - Percentage of planets where intelligent life evolves" />
-          <input type="text" id="Fc" name="Fc" value="" placeholder="Fc - Percentage of civilizations that will develop communication" />
-          <input type="text" id="L" name="L" value="" placeholder="L - Years a civilization will last" />
+          <input type="text" id="R" name="R" value={this.state.R} onChange={this.handleChange} placeholder="R* - Annual formation rate of suitable stars in the Milky Way" />
+          <input type="text" id="Fp" name="Fp" value={this.state.Fp} onChange={this.handleChange} placeholder="Fp - Percentage of stars that have planets" />
+          <input type="text" id="Ne" name="Ne" value={this.state.Ne} onChange={this.handleChange} placeholder="Ne - Number of planets per star that can support life" />
+          <input type="text" id="Fl" name="Fl"  value={this.state.Fl} onChange={this.handleChange} placeholder="Fl - Percentage of planets where life emerges" />
+          <input type="text" id="Fi" name="Fi" value={this.state.Fi}  onChange={this.handleChange}placeholder="Fi - Percentage of planets where intelligent life evolves" />
+          <input type="text" id="Fc" name="Fc" value={this.state.Fc} onChange={this.handleChange} placeholder="Fc - Percentage of civilizations that will develop communication" />
+          <input type="text" id="L" name="L" value={this.state.L}  onChange={this.handleChange}placeholder="L - Years a civilization will last" />
           <input type="submit" value="Calculate" />
       </form>
     
